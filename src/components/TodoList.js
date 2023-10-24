6516efc4f46a983d59a3370ffaa13716f3a18325
Nick Morin <morinnick31@gmail.com>
2023-10-24 20:20:48 +0300

    create new component TodoListItem

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,17 @@
 import React from "react";
+import TodoListItem from "./TodoListItem";
 const TodoList = () => {
   const items = ["Exe. 1", "Exe. 2"];
   return (
     <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
+      {/* <li>{items[0]}</li>
+      <li>{items[1]}</li> */}
+      <li>
+        <TodoListItem />
+      </li>
+      <li>
+        <TodoListItem />
+      </li>
     </ul>
   );
 };
